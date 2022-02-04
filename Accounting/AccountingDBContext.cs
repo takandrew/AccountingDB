7ee42ef22e91d46888ce5838641b4d59715a215a
Takandrew <andreikaril@mail.ru>
2022-02-04 21:26:38 +0300

    Created project and DB

---
+++ Accounting/AccountingDBContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Accounting
+{
+    public partial class AccountingDBContext : DbContext
+    {
+        public AccountingDBContext()
+        {
+        }
+
+        public AccountingDBContext(DbContextOptions<AccountingDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AccountingEntity> AccountingEntities { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=AccountingDB.db;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AccountingEntity>(entity =>
+            {
+                entity.ToTable("AccountingEntity");
+
+                entity.HasIndex(e => e.Id, "IX_AccountingEntity_ID")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Accounting/AccountingEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Accounting
+{
+    public partial class AccountingEntity
+    {
+        public long Id { get; set; }
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public string? Status { get; set; }
+    }
+}
