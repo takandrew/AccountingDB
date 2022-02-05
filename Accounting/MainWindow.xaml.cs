57feffab557122bb2a1f13e4f37ea6fc81ab5a0a
Takandrew <andreikaril@mail.ru>
2022-02-05 15:04:15 +0300

    Added viewing and filtering

--- Accounting/MainWindow.xaml.cs
+++ Accounting/MainWindow.xaml.cs
@@ -15,16 +15,84 @@ using System.Windows.Shapes;
 
 namespace Accounting
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        
+
         public MainWindow()
         {
             InitializeComponent();
-            
-            
+
+            ComboBox_Type.ItemsSource = Queries.Query_Type();
+            ComboBox_Status.ItemsSource = Queries.Query_Status();
+            ComboBox_Type.SelectedItem = "All";
+            ComboBox_Status.SelectedItem = "All";
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            AccTable_Update(true, true);
+        }
+
+        private void AccTable_Update(bool allType, bool allStatus)
+        {
+            AccountingDBContext dbContext = new AccountingDBContext();
+            if (allType && allStatus)
+            {
+                var query =
+            from AccEntity in dbContext.AccountingEntities
+            select new { AccEntity.Name, AccEntity.Type, AccEntity.Status, AccEntity.Progress };
+                AccTable.ItemsSource = query.ToList();
+            }
+            else if (allType && !allStatus)
+            {
+                var query =
+            from AccEntity in dbContext.AccountingEntities
+            where AccEntity.Status == ComboBox_Status.SelectedItem.ToString() 
+            select new { AccEntity.Name, AccEntity.Type, AccEntity.Status, AccEntity.Progress };
+                AccTable.ItemsSource = query.ToList();
+            }
+            else if (!allType && allStatus)
+            {
+                var query =
+            from AccEntity in dbContext.AccountingEntities
+            where AccEntity.Type == ComboBox_Type.SelectedItem.ToString()
+            select new { AccEntity.Name, AccEntity.Type, AccEntity.Status, AccEntity.Progress };
+                AccTable.ItemsSource = query.ToList();
+            }
+            else if (!allType && !allStatus)
+            {
+                var query =
+            from AccEntity in dbContext.AccountingEntities
+            where AccEntity.Type == ComboBox_Type.SelectedItem.ToString() && AccEntity.Status == ComboBox_Status.SelectedItem.ToString()
+            select new { AccEntity.Name, AccEntity.Type, AccEntity.Status, AccEntity.Progress };
+                AccTable.ItemsSource = query.ToList();
+            }
+        }
+
+        private void Button_Filter_Click(object sender, RoutedEventArgs e)
+        {
+            bool allType = false, allStatus = false;
+            if (ComboBox_Type.SelectedItem.ToString() == "All")
+                allType = true;
+            if (ComboBox_Status.SelectedItem.ToString() == "All")
+                allStatus = true;
+            AccTable_Update(allType, allStatus);
+        }
+
+        private void Button_Insert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Accounting/Queries.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting
+{
+    public class Queries
+    {
+        //AccountingDBContext dbContext = new AccountingDBContext();
+
+        public static List<string> Query_Type()
+        {
+            AccountingDBContext dbContext = new AccountingDBContext();
+            var query_Type =
+            (from accountingEntity in dbContext.AccountingEntities
+            select accountingEntity.Type).Distinct();
+            List<string> typeList = new List<string>();
+            typeList.Add("All");
+            foreach (var type in query_Type)
+                typeList.Add(type.ToString());
+            return typeList;
+        }
+
+        public static List<string> Query_Status()
+        {
+            AccountingDBContext dbContext = new AccountingDBContext();
+            var query_Status =
+            (from accountingEntity in dbContext.AccountingEntities
+            select accountingEntity.Status).Distinct();
+            List<string> statusList = new List<string>();
+            statusList.Add("All");
+            foreach (var status in query_Status)
+                statusList.Add(status.ToString());
+            return statusList;
+        }
+
+    }
+}
