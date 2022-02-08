2387d8a1292c8d100d891ebfec799e66bbb4b46b
Takandrew <andreikaril@mail.ru>
2022-02-08 21:33:12 +0300

    Bug fixes

--- Accounting/FileWork.cs
+++ Accounting/FileWork.cs
@@ -13,11 +13,20 @@ namespace Accounting
         {
             string path = @".\AccountingDB.db";
             System.IO.FileInfo fileInf = new System.IO.FileInfo(path);
-            if (fileInf.Exists)
+            try
             {
-                fileInf.CopyTo(newPath, true);
+                if (fileInf.Exists)
+                {
+                    fileInf.CopyTo(newPath, true);
+                    MessageBox.Show("The base has been successfully backed up.", "Backup", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                    MessageBox.Show("The base file wasn't. \nCheck if the «AccountingDB.db» exist in the program directory ", "Backup", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            MessageBox.Show("The base has been successfully backed up.", "Backup", MessageBoxButton.OK, MessageBoxImage.Information);
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         public static void SaveLog(string text)
@@ -41,11 +50,20 @@ namespace Accounting
         {
             string path = @".\BaseLog.txt";
             System.IO.FileInfo fileInf = new System.IO.FileInfo(path);
-            if (fileInf.Exists)
+            try
             {
-                fileInf.CopyTo(newPath, true);
+                if (fileInf.Exists)
+                {
+                    fileInf.CopyTo(newPath, true);
+                    MessageBox.Show("The log has been successfully backed up.", "Backup", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                    MessageBox.Show("The log file wasn't found. \nCheck if the «BaseLog.txt» exist in the program directory.", "Backup", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            MessageBox.Show("The log has been successfully backed up.", "Backup", MessageBoxButton.OK, MessageBoxImage.Information);
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
