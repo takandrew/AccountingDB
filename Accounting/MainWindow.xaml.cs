1f7631aa0a251872fc24029580293e1076f7e395
Takandrew <andreikaril@mail.ru>
2022-04-02 15:41:15 +0300

    Added name search

--- Accounting/MainWindow.xaml.cs
+++ Accounting/MainWindow.xaml.cs
@@ -7,7 +7,9 @@ namespace Accounting
 {
     public partial class MainWindow : Window
     {
-        
+        public bool AllStatus { get; set; }
+        public bool AllType { get; set; }
+        private string NameFilter { get; set; }
 
         public MainWindow()
         {
@@ -22,45 +24,24 @@ namespace Accounting
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            AccTable_Update(true, true);
-            ComboBox_Type.SelectedItem = "All";
-            ComboBox_Status.SelectedItem = "All";
+            NameFilter = "";
+            AllType = true;
+            AllStatus = true;
+            AccTable_Update(AllType, AllStatus, NameFilter);
         }
 
-        private void AccTable_Update(bool allType, bool allStatus)
+        private void AccTable_Update(bool allType, bool allStatus, string nameFilter)
         {
             var dbContext = new AccountingDBContext();
-            if (allType && allStatus)
-            {
-                var query =
-            from AccEntity in dbContext.AccountingEntities
-            select new { AccEntity.Id, AccEntity.Name, AccEntity.Type, AccEntity.Status, AccEntity.Progress };
-                AccTable.ItemsSource = query.ToList();
-            }
-            else if (allType && !allStatus)
-            {
-                var query =
-            from AccEntity in dbContext.AccountingEntities
-            where AccEntity.Status == ComboBox_Status.SelectedItem.ToString()
-            select new { AccEntity.Id, AccEntity.Name, AccEntity.Type, AccEntity.Status, AccEntity.Progress };
-                AccTable.ItemsSource = query.ToList();
-            }
-            else if (!allType && allStatus)
-            {
-                var query =
-            from AccEntity in dbContext.AccountingEntities
-            where AccEntity.Type == ComboBox_Type.SelectedItem.ToString()
-            select new { AccEntity.Id, AccEntity.Name, AccEntity.Type, AccEntity.Status, AccEntity.Progress };
-                AccTable.ItemsSource = query.ToList();
-            }
-            else if (!allType && !allStatus)
-            {
-                var query =
-            from AccEntity in dbContext.AccountingEntities
-            where AccEntity.Type == ComboBox_Type.SelectedItem.ToString() && AccEntity.Status == ComboBox_Status.SelectedItem.ToString()
-            select new { AccEntity.Id, AccEntity.Name, AccEntity.Type, AccEntity.Status, AccEntity.Progress };
-                AccTable.ItemsSource = query.ToList();
-            }
+
+            string selectedType = allType ? "" : ComboBox_Type.SelectedItem.ToString();
+            string selectedStatus = allStatus ? "" : ComboBox_Status.SelectedItem.ToString();
+
+            var query =
+                from accEntity in dbContext.AccountingEntities
+                where accEntity.Type.Contains(selectedType) && accEntity.Status.Contains(selectedStatus) && accEntity.Name.Contains(nameFilter)
+                select new { accEntity.Id, accEntity.Name, accEntity.Type, accEntity.Status, accEntity.Progress };
+            AccTable.ItemsSource = query.ToList();
             AccTable.Columns[0].Width = DataGridLength.Auto;
             AccTable.Columns[2].Width = DataGridLength.Auto;
             AccTable.Columns[3].Width = DataGridLength.Auto;
@@ -68,21 +49,21 @@ namespace Accounting
             UpdateComboboxes();
         }
 
-        private void Button_Filter_Click(object sender, RoutedEventArgs e)
+        private void Filter()
         {
-            bool allType = false, allStatus = false;
+            AllStatus = false; AllType = false;
             if (ComboBox_Type.SelectedItem.ToString() == "All")
-                allType = true;
+                AllType = true;
             if (ComboBox_Status.SelectedItem.ToString() == "All")
-                allStatus = true;
-            AccTable_Update(allType, allStatus);
+                AllStatus = true;
+            AccTable_Update(AllType, AllStatus, NameFilter);
         }
 
         private void Button_Insert_Click(object sender, RoutedEventArgs e)
         {
             var inserting = new Inserting();
             inserting.ShowDialog();
-            AccTable_Update(true, true);
+            AccTable_Update(AllType, AllStatus, NameFilter);
         }
 
         private void Button_Update_Click(object sender, RoutedEventArgs e)
@@ -95,7 +76,7 @@ namespace Accounting
             {
                 MessageBox.Show("You should select record before updating it.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            AccTable_Update(true, true);
+            AccTable_Update(AllType, AllStatus, NameFilter);
         }
 
         private void Button_Delete_Click(object sender, RoutedEventArgs e)
@@ -108,7 +89,7 @@ namespace Accounting
             {
                 MessageBox.Show("You should select record before deleting it.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            AccTable_Update(true, true);
+            AccTable_Update(AllType, AllStatus, NameFilter);
         }
 
         private void Button_BackupBase_Click(object sender, RoutedEventArgs e)
@@ -134,12 +115,30 @@ namespace Accounting
         private void Button_ApplyTheBaseBackup_Click(object sender, RoutedEventArgs e)
         {
             FileWork.ApplyTheBaseBackup();
-            AccTable_Update(true, true);
+            AccTable_Update(AllType, AllStatus, NameFilter);
         }
 
         private void Button_ApplyTheLogBackup_Click(object sender, RoutedEventArgs e)
         {
             FileWork.ApplyTheLogBackup();
+        }
+
+        private void ComboBox_Status_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox_Type.SelectedItem != null && ComboBox_Status.SelectedItem != null)
+                Filter();
+        }
+
+        private void ComboBox_Type_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox_Type.SelectedItem != null && ComboBox_Status.SelectedItem != null) 
+                Filter();
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            NameFilter = SearchTextBox.Text;
+            AccTable_Update(AllType, AllStatus, NameFilter);
         }
     }
 }
